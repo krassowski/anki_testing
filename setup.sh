f34987b91105e0a8e0754784f412b5867f26457e
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-06-14 15:46:20 +0800

    install pyqt5 as apt package, try running anki_testing/run_tests.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
-sudo apt-get install portaudio19-dev xvfb git
-python3 -m pip install PyQt5==5.15 sip
+sudo apt-get install portaudio19-dev xvfb python3-pyqt5
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
 export DISPLAY=:99
 source install_anki.sh
