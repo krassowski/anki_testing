660cd24453027a64f939d0c6026d1b2920d06645
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-06-14 15:13:13 +0800

    install from anki's setup.py

--- install_anki.sh
+++ install_anki.sh
@@ -6,4 +6,7 @@ if [ ! -e "$anki_dir" ]
 then
     echo "$anki_dir not detected, cloning from master"
     git clone https://github.com/ankitects/anki anki_root
+    echo "installing from anki's setup.py"
+    cd anki_root/qt
+    python3 setup.py install
 fi
