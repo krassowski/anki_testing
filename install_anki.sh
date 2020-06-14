9650e2e887a372edc91434c0ecf3c8406f1caf75
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-06-14 15:19:18 +0800

    need to install both pylib/anki and aqt

--- install_anki.sh
+++ install_anki.sh
@@ -6,7 +6,13 @@ if [ ! -e "$anki_dir" ]
 then
     echo "$anki_dir not detected, cloning from master"
     git clone https://github.com/ankitects/anki anki_root
-    echo "installing from anki's setup.py"
+    echo "installing anki module from anki local checkout"
+    cd anki_root/pylib
+    python3 setup.py install
+    cd ..
+    echo "done installing anki module"
+    echo "installing aqt module from anki local checkout"
     cd anki_root/qt
     python3 setup.py install
+    echo "done installing aqt module"
 fi
