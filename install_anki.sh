b9b6409abcef4004dcb91df610be85722c3ad153
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-06-14 15:27:13 +0800

    fix directory issue when installing both of anki's modules

--- install_anki.sh
+++ install_anki.sh
@@ -2,17 +2,20 @@
 
 anki_dir='anki_root'
 
+
+
 if [ ! -e "$anki_dir" ]
 then
     echo "$anki_dir not detected, cloning from master"
     git clone https://github.com/ankitects/anki anki_root
-    echo "installing anki module from anki local checkout"
-    cd anki_root/pylib
+    cd anki_root
+    echo "installing anki module (pylib) from anki local checkout"
+    cd pylib
     python3 setup.py install
     cd ..
     echo "done installing anki module"
-    echo "installing aqt module from anki local checkout"
-    cd anki_root/qt
+    echo "installing aqt module (qt) from anki local checkout"
+    cd qt
     python3 setup.py install
     echo "done installing aqt module"
 fi
