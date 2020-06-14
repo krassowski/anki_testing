0d521c35329e1941dd86680e25425d9deecdab25
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-06-14 16:18:46 +0800

    no need to checkout anki repo, we install from pypi instead

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ sudo apt-get update
 sudo apt-get install portaudio19-dev xvfb
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
 export DISPLAY=:99
-#source install_anki.sh
