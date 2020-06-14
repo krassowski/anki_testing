c075b0b5603f55477e724c961b404dd3aaa29e42
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-06-14 15:54:00 +0800

    install pyqt5 explicitely, run pytest directly

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install portaudio19-dev xvfb python3-pyqt5
+sudo apt-get install portaudio19-dev xvfb
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
 export DISPLAY=:99
 source install_anki.sh
